179551dd486b081066f4836d75ba041a1fc36b91
Admin Stagiaire <Admin Stagiaire@P3570-7D6Q>
2022-11-24 12:15:20 +0100

    fin création clas

--- projet_dawan/Model/Episode.cs
+++ projet_dawan/Model/Episode.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace projet_dawan.Model
 {
-    internal class Episode
+    public class Episode
     {
-    }
+		private int id;
+		private Saison saison_id;
+		private string nom;
+		private string resume;
+		private DateTime date_prem_diff;
+
+		public DateTime DatePremDiff
+		{
+			get { return date_prem_diff; }
+			set { date_prem_diff = value; }
+		}
+
+
+		public string Resume
+		{
+			get { return resume; }
+			set { resume = value; }
+		}
+
+
+		public string Nom
+		{
+			get { return nom; }
+			set { nom = value; }
+		}
+
+
+		public Saison SaisonId
+		{
+			get { return saison_id; }
+			set { saison_id = value; }
+		}
+
+
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+		public Episode() { }
+
+	}
 }
