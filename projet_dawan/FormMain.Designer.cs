d45adeffdfc1f8416e63e6f7a143a3675a6a6141
Zookan <enzo.onesti@gmail.com>
2022-11-24 14:42:47 +0100

    Test commit enzo

--- projet_dawan/FormMain.Designer.cs
+++ projet_dawan/FormMain.Designer.cs
@@ -29,11 +29,12 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.btnConnexion = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(354, 148);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 29);
             this.button1.TabIndex = 0;
@@ -41,11 +42,21 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // btnConnexion
+            // 
+            this.btnConnexion.Location = new System.Drawing.Point(296, 309);
+            this.btnConnexion.Name = "btnConnexion";
+            this.btnConnexion.Size = new System.Drawing.Size(139, 52);
+            this.btnConnexion.TabIndex = 1;
+            this.btnConnexion.Text = "Connexion";
+            this.btnConnexion.UseVisualStyleBackColor = true;
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConnexion);
             this.Controls.Add(this.button1);
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
@@ -57,5 +68,6 @@
         #endregion
 
         private Button button1;
+        private Button btnConnexion;
     }
 }
