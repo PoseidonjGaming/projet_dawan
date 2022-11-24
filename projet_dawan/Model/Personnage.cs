38f1880e033540eecbf76a86e27938d8946c60c1
Admin Stagiaire <Admin Stagiaire@P3570-7D6Q>
2022-11-24 12:03:49 +0100

    modif class acteur_id int -> Acteur

--- projet_dawan/Model/Personnage.cs
+++ projet_dawan/Model/Personnage.cs
@@ -1,6 +1,43 @@
 ﻿namespace projet_dawan.Model
 {
-    internal class Personnage
+    public class Personnage
     {
-    }
+		private int id;
+		private string nom;
+		private int acteur_id;
+		private int serie_id;
+
+		public int SerieId
+		{
+			get { return serie_id; }
+			set { serie_id = value; }
+		}
+
+
+		public int ActeurId
+		{
+			get { return acteur_id; }
+			set { acteur_id = value; }
+		}
+
+
+		public string Nom
+		{
+			get { return nom; }
+			set { nom = value; }
+		}
+
+
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+		public Personnage()
+		{
+
+		}
+
+	}
 }
