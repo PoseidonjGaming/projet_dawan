c8e51fcdf05fa3b4143a28532c00768df660223c
Admin Stagiaire <Admin Stagiaire@P3570-7D6Q>
2022-11-24 12:15:05 +0100

    cr&ation class episode

--- projet_dawan/Model/Saison.cs
+++ projet_dawan/Model/Saison.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace projet_dawan.Model
 {
-    internal class Saison
+    public class Saison
     {
-    }
+		private int id;
+		private Int16 num;
+		private int nb_episode;
+		private Serie serie_Id;
+
+		public Serie SerieId
+		{
+			get { return serie_Id; }
+			set { serie_Id = value; }
+		}
+
+
+		public int NbEpisode
+		{
+			get { return nb_episode; }
+			set { nb_episode = value; }
+		}
+
+
+		public Int16 Num
+		{
+			get { return num; }
+			set { num = value; }
+		}
+
+
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+		public Saison() { }
+
+	}
 }
