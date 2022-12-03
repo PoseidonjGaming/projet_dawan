5c9b37e1671aed42d01f779cbb69d51a7bdba17b
PoseidonjGaming <lucasmagnier@free.fr>
2022-12-03 11:21:25 +0100

    up

---
+++ SerieEFDLL/Model/Acteur.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projet_dawan.Model
+{
+    public class Acteur
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nom { get; set; }
+        [Required]
+        public string Prenom { get; set; }
+
+        public Acteur(int id, string nom, string prenom)
+        {
+            Id = id;
+            Nom = nom;
+            Prenom = prenom;
+        }
+    }
+}
