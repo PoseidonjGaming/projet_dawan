0dfbf86a3bfe503f9d2cfb29164d1f378ebc8469
PoseidonjGaming <lucasmagnier@free.fr>
2022-12-03 11:54:15 +0100

    up 2

---
+++ projet_dawan/ActeurContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using projet_dawan.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projet_dawan
+{
+    internal class ActeurContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Properties.Settings.Default.Connection);
+        }
+
+        public DbSet<Acteur> Acteurs { get; set; }
+    }
+}
---
+++ projet_dawan/Forms/FormAddActeur.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace projet_dawan.Forms
+{
+    partial class FormAddActeur
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNom = new System.Windows.Forms.TextBox();
+            this.textBoxPrenom = new System.Windows.Forms.TextBox();
+            this.labelNom = new System.Windows.Forms.Label();
+            this.labelPrenom = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxNom
+            // 
+            this.textBoxNom.Location = new System.Drawing.Point(76, 12);
+            this.textBoxNom.Name = "textBoxNom";
+            this.textBoxNom.Size = new System.Drawing.Size(100, 23);
+            this.textBoxNom.TabIndex = 0;
+            // 
+            // textBoxPrenom
+            // 
+            this.textBoxPrenom.Location = new System.Drawing.Point(76, 41);
+            this.textBoxPrenom.Name = "textBoxPrenom";
+            this.textBoxPrenom.Size = new System.Drawing.Size(100, 23);
+            this.textBoxPrenom.TabIndex = 1;
+            // 
+            // labelNom
+            // 
+            this.labelNom.AutoSize = true;
+            this.labelNom.Location = new System.Drawing.Point(15, 15);
+            this.labelNom.Name = "labelNom";
+            this.labelNom.Size = new System.Drawing.Size(40, 15);
+            this.labelNom.TabIndex = 2;
+            this.labelNom.Text = "Nom: ";
+            // 
+            // labelPrenom
+            // 
+            this.labelPrenom.AutoSize = true;
+            this.labelPrenom.Location = new System.Drawing.Point(15, 45);
+            this.labelPrenom.Name = "labelPrenom";
+            this.labelPrenom.Size = new System.Drawing.Size(55, 15);
+            this.labelPrenom.TabIndex = 3;
+            this.labelPrenom.Text = "Prénom: ";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(15, 70);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(161, 23);
+            this.buttonAdd.TabIndex = 4;
+            this.buttonAdd.Text = "Ajouter";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // FormAddActeur
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(195, 110);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.labelPrenom);
+            this.Controls.Add(this.labelNom);
+            this.Controls.Add(this.textBoxPrenom);
+            this.Controls.Add(this.textBoxNom);
+            this.Name = "FormAddActeur";
+            this.Text = "FormAddActeur";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBoxNom;
+        private TextBox textBoxPrenom;
+        private Label labelNom;
+        private Label labelPrenom;
+        private Button buttonAdd;
+    }
+}
