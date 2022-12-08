853c68250519c097872548b843b8d080916e5df2
PoseidonjGaming <lucasmagnier@free.fr>
2022-12-08 08:50:53 +0100

    cnx

--- SerieDLL_EF/Models/BddprojetContext.cs
+++ SerieDLL_EF/Models/BddprojetContext.cs
@@ -27,7 +27,7 @@ public partial class BddprojetContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer("Data Source=P7710-2VGQK72\\SQLEXPRESS;Initial Catalog=serie_list;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BDDProjet;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
