d7f30a25b810264ab704b31da0c9d4ccba538160
Admin Stagiaire <Admin Stagiaire@P3570-7D6Q>
2022-12-13 13:24:46 +0100

    logique accueil décentraliser

--- projet_dawan/Forms/FormAccueil.cs
+++ projet_dawan/Forms/FormAccueil.cs
@@ -1,4 +1,5 @@
-﻿using projet_dawan.Forms;
+﻿using projet_dawan.FormLogique;
+using projet_dawan.Forms;
 using projet_dawan.Models;
 using projet_dawan_WinForm;
 using SerieDLL_EF.Models;
@@ -9,15 +10,13 @@ namespace projet_dawan
 {
     public partial class FormAccueil : Form
     {
-        private string path = Directory.GetCurrentDirectory() + "\\photo\\";
-        private List<Serie> serieList = new List<Serie>();
-        private UserApp user;
+        Accueil Accueil;
         public FormAccueil()
         {
             InitializeComponent();
             StartPosition = FormStartPosition.CenterScreen;
             FormBorderStyle = FormBorderStyle.FixedSingle;
-            user=Properties.Settings.Default.UserRemain;
+            Accueil = new(this);
         }
 
         private void toolStripAddSerie_Click(object sender, EventArgs e)
@@ -41,146 +40,50 @@ namespace projet_dawan
         private void toolStripBibli_Click(object sender, EventArgs e)
         {
             //affiche la page de la bibliothéque des séries de la base de données
-            OpenFormBibli(string.Empty);
+            Accueil.Biblio_Click();
         }
 
         private void quitterToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            var quitter = MessageBox.Show("Etes vous sur de vouloir vous quitter l'application ?", "Quitter ?",
-                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            if (quitter == DialogResult.Yes)
-            {
-                Application.Exit();
-            }
+            Accueil.Quitter_Click();
         }
 
         private void toolStripDeco_Click(object sender, EventArgs e)
         {
-            var deconnecter = MessageBox.Show("Etes vous sur de vouloir vous déconnecter de ce compte ?", "Se déconnecter ?",
-                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            if (deconnecter == DialogResult.Yes)
-            {
-                Properties.Settings.Default.token = string.Empty;
-                Properties.Settings.Default.UserRemain = null;
-                Properties.Settings.Default.Save();
-                this.Close();
-            }
+            Accueil.Deco_Click();
 
         }
 
         private void toolStripGerer_Click(object sender, EventArgs e)
         {
-            // affiche la page de gestion du compte
-            if(user.Roles>Roles.User)
-            {
-                FormGererCompte compte = new();
-
-                compte.ShowDialog();
-            }
-            
+            Accueil.Gerer_Click();
         }
 
         private void watchlistToolStripMenuItem_Click(object sender, EventArgs e)
         {
             // affiche la watchlist liée au compte actuel
-            FormWatchlist watchlist = new();
-
-            watchlist.ShowDialog();
+            Accueil.WatchList_Click();
         }
 
        
 
         private void PageAcceuil_Load(object sender, EventArgs e)
         {
-            SerieService service = new();
-            serieList = service.GetAll();
-            
-            Populate(4);
-        }
-
-        private void Serie_Click(object sender, EventArgs e)
-        {
-            Button? button = sender as Button;
-            if (button != null)
-            {
-                Serie? serie = serieList.Find(s => s.Nom == button.Text);
-                if (serie != null)
-                {
-                    FormSerie formSerie = new(serie);
-                    formSerie.ShowDialog(this);
-
-                }
-
-            }
-            else
-            {
-                PictureBox? pictureBox = sender as PictureBox;
-                Serie? serie = serieList.Find(s => s.Nom == pictureBox.Name);
-                if (serie != null)
-                {
-                    FormSerie formSerie = new(serie);
-                    formSerie.ShowDialog(this);
-                }
-            }
-        }
-
-     
-
-        private void Populate(int I)
-        {
-            for (int i = 0; i < I; i++)
-            {
-                PictureBox pictureBox = new PictureBox();
-                pictureBox.Location = new Point((95 * i * 2) + 25, 50);
-                pictureBox.Size = new Size(138, 170);
-                pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
-                pictureBox.Name = serieList[i].Nom;
-                pictureBox.ImageLocation = path + serieList[i].Affiche;
-                pictureBox.Click += Serie_Click;
-
-                this.Controls[this.Controls.IndexOf(groupBox1)].Controls.Add(pictureBox);
-
-                Button btnSerie = new Button();
-                btnSerie.Location = new Point((95 * i * 2) + 25, 225);
-                btnSerie.Size = new Size(138, 50);
-                btnSerie.Text = serieList[i].Nom;
-                btnSerie.Click += Serie_Click;
-                this.Controls[this.Controls.IndexOf(groupBox1)].Controls.Add(btnSerie);
-            }
+            Accueil.Load();   
         }
 
         private void btnSearch_Click(object sender, EventArgs e)
         {
-            OpenFormBibli(txtRechercher.Text);
+            Accueil.Search_Clock();
         }
 
-        private void form_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            this.Show();
-        }
+       
 
-        private void OpenFormBibli(string text)
-        {
-            
-            SerieService service = new();
-            FormBibliotheque formBibliotheque = new(text);
-            formBibliotheque.FormClosing += form_FormClosing;
-            this.Hide();
-            formBibliotheque.ShowDialog(this);
-        }
+        
 
         private void gererLesActeursToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (user.Roles == Roles.SuperAdmin)
-            {
-                FormManageActeur formManageActeur = new();
-                formManageActeur.ShowDialog(this);
-            }
-            else
-            {
-                MessageBox.Show("Test");
-            }
-           
+            Accueil.GererLesActeurs_Click();
         }
     }
 }
