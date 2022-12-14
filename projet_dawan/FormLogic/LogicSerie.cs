00e5d6628136914b9700e261620bf88f33e87a31
Admin Stagiaire <Admin Stagiaire@P3570-7D6Q>
2022-12-14 13:09:06 +0100

    logic form série décentralisée

---
+++ projet_dawan/FormLogic/LogicSerie.cs
@@ -0,0 +1,77 @@
+﻿using projet_dawan_WinForm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using projet_dawan.Models;
+using SerieDLL_EF.Service;
+using projet_dawan.Forms;
+
+namespace projet_dawan.FormLogic
+{
+    internal class LogicSerie
+    {
+        private Serie Serie = new();
+        private List<Saison> Saisons = new List<Saison>();
+        public FormSerie Form { get; set; }
+
+        public LogicSerie(FormSerie form)
+        {
+            Form = form;
+            
+        }
+
+        public void Load(Serie serie)
+        {
+            Serie = serie;
+            Form.lblSerie.Text = Serie.Nom;
+            Form.lblSerie.Location = new Point((Form.Size.Width - Form.lblSerie.Size.Width) / 2, Form.lblSerie.Location.Y);
+            Form.lblDateSerie.Text = Serie.DateDiff.ToString();
+            Form.linkLblBASerie.Text = Serie.UrlBa;
+            Form.txtBoxResumeSerie.Text = Serie.Resume;
+            Form.pictureBoxSerie.ImageLocation = Directory.GetCurrentDirectory() + "\\Photo\\" + Serie.Affiche;
+            SaisonService service = new SaisonService();
+            foreach (Saison saison in service.GetSaisonsBySerie(Serie.Id))
+            {
+                Saisons.Add(saison);
+                Form.lstBoxSaison.Items.Add(saison.Numero.ToString());
+            }
+        }
+
+        public void BtnCasting_Click()
+        {
+            PersonnageService service = new PersonnageService();
+            FormCasting casting = new(service.GetBySerie(Serie.Id));
+            casting.ShowDialog();
+        }
+
+        public void LstBoxSaison_SelectedIndexChanged()
+        {
+            if (Form.lstBoxSaison.SelectedIndex != -1)
+            {
+                Saison saison = Saisons[Form.lstBoxSaison.SelectedIndex];
+                saison.Serie = Serie;
+                FormEpisode formSaison = new(saison);
+                formSaison.FormClosed += OpenForm;
+                Form.Hide();
+                formSaison.ShowDialog(Form);
+            }
+        }
+
+        public void ButtonToWatch_Click()
+        {
+            if (!Properties.Settings.Default.UserRemain.ToWatch.Contains(Serie.Id))
+            {
+
+                Properties.Settings.Default.UserRemain.ToWatch.Add(Serie.Id);
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void OpenForm(object sender, EventArgs e)
+        {
+            Form.Show();
+        }
+    }
+}
--- projet_dawan/Forms/FormSerie.cs
+++ projet_dawan/Forms/FormSerie.cs
@@ -1,3 +1,4 @@
+using projet_dawan.FormLogic;
 using projet_dawan.Forms;
 using projet_dawan.Models;
 using SerieDLL_EF.Repository;
@@ -7,68 +8,39 @@ namespace projet_dawan_WinForm
 {
     public partial class FormSerie : Form
     {
-        private Serie Serie = new();
-        private List<Saison> Saisons = new List<Saison>();
 
+        private LogicSerie logicSerie;
         public FormSerie(Serie serie)
         {
             InitializeComponent();
-            Serie = serie;
-            //(width - 176) / 2
+            logicSerie = new(this);
+            logicSerie.Load(serie);
+            
         }
 
         private void FormSerie_Load(object sender, EventArgs e)
         {
             
 
-            lblSerie.Text = Serie.Nom;
-            lblSerie.Location = new Point((this.Size.Width-lblSerie.Size.Width) / 2, lblSerie.Location.Y);
-            lblDateSerie.Text = Serie.DateDiff.ToString();
-            linkLblBASerie.Text = Serie.UrlBa;
-            txtBoxResumeSerie.Text = Serie.Resume;
-            pictureBoxSerie.ImageLocation = Directory.GetCurrentDirectory() + "\\Photo\\" + Serie.Affiche;
-            SaisonService service = new SaisonService();
-            foreach (Saison saison in service.GetSaisonsBySerie(Serie.Id))
-            {
-                Saisons.Add(saison);
-                lstBoxSaison.Items.Add(saison.Numero.ToString());
-            }
+           
         }
 
         private void btnCasting_Click(object sender, EventArgs e)
         {
             // retour form casting correspondant au personnage présent dans la série
-            PersonnageService service= new PersonnageService();
-            FormCasting casting = new(service.GetBySerie(Serie.Id));
-            casting.ShowDialog();
+            logicSerie.BtnCasting_Click();
         }
 
         private void lstBoxSaison_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (lstBoxSaison.SelectedIndex != -1)
-            {
-                Saison saison = Saisons[lstBoxSaison.SelectedIndex];
-                saison.Serie = Serie;
-                FormEpisode formSaison = new(saison);
-                formSaison.FormClosed += OpenForm;
-                this.Hide();
-                formSaison.ShowDialog(this);
-            }
+            logicSerie.LstBoxSaison_SelectedIndexChanged();
         }
 
-        private void OpenForm(object sender, EventArgs e)
-        {
-            this.Show();
-        }
+       
 
         private void buttonToWatch_Click(object sender, EventArgs e)
         {
-            if (!projet_dawan.Properties.Settings.Default.UserRemain.ToWatch.Contains(Serie.Id))
-            {
-
-                projet_dawan.Properties.Settings.Default.UserRemain.ToWatch.Add(Serie.Id);
-                projet_dawan.Properties.Settings.Default.Save();
-            }
+            logicSerie.ButtonToWatch_Click();
         }
     }
 }
