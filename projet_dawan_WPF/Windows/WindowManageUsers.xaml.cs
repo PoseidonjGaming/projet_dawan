6a7f11a5b56a134eef90ed6eee1eca383d93ebfa
Poseidonj <lucasmagnier@free.fr>
2023-01-04 09:09:40 +0100

    logic manage user externe

--- projet_dawan_WPF/Windows/WindowManageUsers.xaml.cs
+++ projet_dawan_WPF/Windows/WindowManageUsers.xaml.cs
@@ -1,4 +1,5 @@
-﻿using SerieDLL_EF.Models;
+﻿using projet_dawan_WPF.Logic;
+using SerieDLL_EF.Models;
 using SerieDLL_EF.Service;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,106 +15,34 @@ namespace projet_dawan_WPF.Window
     /// </summary>
     public partial class WindowManageUsers : System.Windows.Window
     {
-        private UserService service = new();
-        private List<UserApp> userList = new();
-        private List<Roles> rolesList = new() { Roles.User, Roles.Admin, Roles.SuperAdmin };
+        private LogicManageUsers logic;
         public WindowManageUsers()
         {
             InitializeComponent();
-
-            foreach (Roles role in rolesList)
-            {
-                cmbBoxRoles.Items.Add(role.ToString());
-            }
-
-            userList = service.GetAll();
-            foreach (UserApp user in userList)
-            {
-                lstBoxUsers.Items.Add(user.Login);
-            }
+            logic = new(this);
+            logic.Load();
         }
 
         private void btnAdd_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            
-            if (txtBoxLogin.Text!=string.Empty && txtBoxPWD.Text!=string.Empty && txtBoxPWDConfirm.Text!=string.Empty)
-            {
-                if(txtBoxPWD.Text==txtBoxPWDConfirm.Text)
-                {
-                    if (lstBoxUsers.SelectedIndex != -1)
-                    {
-                        UserApp user = userList[lstBoxUsers.SelectedIndex];
-                        if (user.Equals(Properties.Settings.Default.UserRemain))
-                        {
-                            MessageBox.Show("Vous devez être connecter à un autre compte pour pouvoir modifier ce compte");
-                            return;
-                        }
-                        
-                        user.Login = txtBoxLogin.Text;
-                        user.Password = txtBoxPWDConfirm.Text;
-
-                        service.Update(user);
-
-                    }
-                    else
-                    {
-                        UserApp user = new()
-                        {
-                            Login = txtBoxLogin.Text,
-                            Password = txtBoxPWDConfirm.Text,
-                            Roles = rolesList[cmbBoxRoles.SelectedIndex],
-                        };
-                        service.Add(user);
-                    }
-                    Clear();
-                    Populate();
-                }
-            }
+            logic.BtnAdd_Click();
         }
 
         private void btnCancel_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            Clear();
+            logic.BtnCancel_Click();
         }
 
         private void btnDelete_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            var annuler = MessageBox.Show("Etes vous sur de vouloir annuler l'ajout ?", "Annuler ajout ?",
-               MessageBoxButton.YesNo, MessageBoxImage.Question);
-            if (annuler == MessageBoxResult.Yes)
-            {
-                service.Delete(userList[lstBoxUsers.SelectedIndex]);
-            }
-            Populate();
+            logic.BtnDelete_Click();
         }
 
         private void lstBoxUsers_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
-            if (lstBoxUsers.SelectedIndex != -1)
-            {
-                txtBoxLogin.Text = userList[lstBoxUsers.SelectedIndex].Login;
-                cmbBoxRoles.SelectedIndex = cmbBoxRoles.Items.IndexOf(rolesList.Find(r => r.ToString() == userList[lstBoxUsers.SelectedIndex].Roles.ToString()));
-            }
+            logic.LstBoxUsers_SelectionChanged();
         }
 
-        private void Populate()
-        {
-            Clear();
-            lstBoxUsers.Items.Clear();
-            userList = service.GetAll();
-            foreach (UserApp user in userList)
-            {
-                lstBoxUsers.Items.Add(user.Login);
-            }
-        }
-
-        private void Clear()
-        {
-            txtBoxLogin.Clear();
-            txtBoxPWD.Clear();
-            txtBoxPWDConfirm.Clear();
-            cmbBoxRoles.SelectedIndex = -1;
-            lstBoxUsers.SelectedIndex = -1;
-        }
+       
     }
 }
