b730d1dfe76efe137ac51977905226f75606a253
Poseidonj <lucasmagnier@free.fr>
2023-01-13 16:13:45 +0100

    base repo

--- SerieDLL_EF/Repository/ActeurRepository.cs
+++ SerieDLL_EF/Repository/ActeurRepository.cs
@@ -1,5 +1,7 @@
-﻿using SerieDLL_EF.Interface;
+﻿using Microsoft.EntityFrameworkCore;
+using SerieDLL_EF.Interface;
 using SerieDLL_EF.Models;
+using System.Formats.Asn1;
 using System.Linq.Expressions;
 using static System.Net.Mime.MediaTypeNames;
 
@@ -10,33 +12,55 @@ namespace SerieDLL_EF.Repository
      * Dans notre cas, elles implèment les interfaces du dossier Interface, pour plus de simplicité
      * Cette classe implémente les interfaces IRepoCRUD et IRepSpecials pour gérer la table acteur
      */
-    public class ActeurRepository : BaseRepo<Acteur>,  IRepoCRUD<Acteur>, IRepSpecials<Acteur>
+    public class ActeurRepository : IRepoCRUD<Acteur>, IRepSpecials<Acteur>
     {
-        public ActeurRepository():base()
+        private readonly BaseRepo<Acteur> BaseRepo;
+        public ActeurRepository()
         {
-            DbSet = Context.Acteurs;
+            BaseRepo = new BaseRepo<Acteur>();
+            BaseRepo.DbSet = BaseRepo.Context.Acteurs;
+        }
+
+        public List<Acteur> GetAll()
+        {
+            return BaseRepo.GetAll();
         }
 
         public Acteur GetById(int id)
         {
-            return GetById(p=> p.Id==id);
+            return BaseRepo.GetById(p => p.Id == id);
         }
 
         public List<Acteur> GetByTxt(string txt)
         {
-            return GetByTxt(a => a.Nom.Contains(txt) || a.Prenom.Contains(txt));
+            return BaseRepo.GetByTxt(a => a.Nom.Contains(txt) || a.Prenom.Contains(txt));
         }
 
         public bool CompareTo(Acteur item)
         {
-            return CompareTo(a => a.Nom == item.Nom && a.Prenom == item.Prenom);
+            return BaseRepo.CompareTo(a => a.Nom == item.Nom && a.Prenom == item.Prenom);
         }
 
         public Acteur? GetCompareTo(Acteur item)
         {
-            return GetCompareTo(a => a.Nom == item.Nom && a.Prenom == item.Prenom);
+            return BaseRepo.GetCompareTo(a => a.Nom == item.Nom && a.Prenom == item.Prenom);
         }
 
-        
+        public void Add(Acteur item)
+        {
+            BaseRepo.Add(item);
+        }
+
+        public void Update(Acteur item)
+        {
+            BaseRepo.Update(item);
+        }
+
+        public void Delete(Acteur item)
+        {
+            BaseRepo.Delete(item);
+        }
+
+
     }
 }
--- SerieDLL_EF/Repository/BaseRepo.cs
+++ SerieDLL_EF/Repository/BaseRepo.cs
@@ -8,59 +8,60 @@ using System.Threading.Tasks;
 
 namespace SerieDLL_EF.Repository
 {
-    public class BaseRepo<TClass> where TClass : class
+    internal class BaseRepo<TClass> where TClass : class
     {
-        protected DbSet<TClass> DbSet;
-        protected BddprojetContext Context;
+        public DbSet<TClass> DbSet;
+        public BddprojetContext Context;
 
         public BaseRepo()
         {
             Context= new BddprojetContext();
         }
 
-        public void Add(TClass item)
+        internal void Add(TClass item)
         {
             DbSet.Add(item);
             Context.SaveChanges();
         }
 
-        public void Update(TClass item)
+        internal void Update(TClass item)
         {
             DbSet.Update(item);
             Context.SaveChanges();
         }
 
-        public void Delete(TClass item)
+        internal void Delete(TClass item)
         {
             DbSet.Remove(item);
             Context.SaveChanges();
         }
 
-        public List<TClass> GetAll()
+        internal List<TClass> GetAll()
         {
             return DbSet.ToList();
         }
 
-        protected TClass GetById(Expression<Func<TClass, bool>> predicate)
+        internal TClass GetById(Expression<Func<TClass, bool>> predicate)
         {
             return DbSet.Where(predicate).ToList()[0];
         }
 
-        protected List<TClass> GetByTxt(Expression<Func<TClass, bool>> predicate)
+        internal List<TClass> GetByTxt(Expression<Func<TClass, bool>> predicate)
         {
             return DbSet.Where(predicate).ToList();
         }
 
-        protected bool CompareTo(Expression<Func<TClass, bool>> predicate)
+        internal bool CompareTo(Expression<Func<TClass, bool>> predicate)
         {
             TClass? acteur = DbSet.Where(predicate).FirstOrDefault();
             return acteur != null;
         }
 
-        public TClass? GetCompareTo(Expression<Func<TClass, bool>> predicate)
+        internal TClass? GetCompareTo(Expression<Func<TClass, bool>> predicate)
         {
             return DbSet.Where(predicate).FirstOrDefault();
         }
 
+        
     }
 }
