2022dc7050596d14b8e6541d034a088d9c1d5f57
Poseidonj <lucasmagnier@free.fr>
2023-01-13 16:56:06 +0100

    publish test

--- projet_dawan_WPF/Logic/Export/LogicExportSerie.cs
+++ projet_dawan_WPF/Logic/Export/LogicExportSerie.cs
@@ -85,7 +85,6 @@ namespace projet_dawan_WPF.Logic.Export
                 foreach (Serie serie in Properties.Settings.Default.ExportSerie)
                 {
                     serie.ShouldExportPersonnage = true;
-                    serie.ShouldExportSaison = false;
                     serie.Personnages = service.GetBySerie(serie.Id);
                 }
             }
@@ -107,11 +106,10 @@ namespace projet_dawan_WPF.Logic.Export
             if ((bool)Window.checkBoxEp.IsChecked)
             {
                 SaisonService saisonService = new();
-                Properties.Settings.Default.ExportSerie = service.GetAll();
                 foreach (Serie serie in Properties.Settings.Default.ExportSerie)
                 {
-                    Properties.Settings.Default.ExportSaison = saisonService.GetSaisonsBySerie(serie.Id);
                     serie.ShouldExportSaison = true;
+                    Properties.Settings.Default.ExportSaison = saisonService.GetSaisonsBySerie(serie.Id);
                     ExportEpisode();
                     serie.Saisons = Properties.Settings.Default.ExportSaison;
                 }
