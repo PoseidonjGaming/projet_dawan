1c59a49557380803667d064b41570754ff298b32
Lucas Magnier <73874300+PoseidonjGaming@users.noreply.github.com>
2023-01-21 15:59:36 +0100

    test baseRepo

--- SerieDLL_EF/Repository/ActeurRepository.cs
+++ SerieDLL_EF/Repository/ActeurRepository.cs
@@ -9,8 +9,12 @@ namespace SerieDLL_EF.Repository
      * Dans notre cas, elles implèment les interfaces du dossier Interface, pour plus de simplicité
      * Cette classe implémente les interfaces IRepoCRUD et IRepSpecials pour gérer la table acteur
      */
-    public class ActeurRepository :  IRepoCRUD<Acteur>, IRepSpecials<Acteur>
+    public class ActeurRepository : BaseRepo<Acteur>, IRepoCRUD<Acteur>, IRepSpecials<Acteur>
     {
+        public ActeurRepository():base()
+        {
+
+        }
         public List<Acteur> GetAll()
         {
             using BddprojetContext context = new();
@@ -34,26 +38,7 @@ namespace SerieDLL_EF.Repository
             return context.Acteurs.Where(a => a.Id == acteur_id).ToList()[0];
         }
 
-        public void Add(Acteur item)
-        {
-            using BddprojetContext context = new();
-            context.Acteurs.Add(item);
-            context.SaveChanges();
-        }
-
-        public void Update(Acteur item)
-        {
-            using BddprojetContext context = new();
-            context.Acteurs.Update(item);
-            context.SaveChanges();
-        }
-
-        public void Delete(Acteur item)
-        {
-            using BddprojetContext context = new();
-            context.Acteurs.Remove(item);
-            context.SaveChanges();
-        }
+       
 
         public bool CompareTo(Acteur item)
         {
