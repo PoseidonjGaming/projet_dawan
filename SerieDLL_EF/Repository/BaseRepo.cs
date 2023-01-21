7055d7a656a81ccc0d79fd7c425c0774b15f8bde
Lucas Magnier <73874300+PoseidonjGaming@users.noreply.github.com>
2023-01-21 15:59:36 +0100

    test baseRepo

---
+++ SerieDLL_EF/Repository/BaseRepo.cs
@@ -0,0 +1,40 @@
+﻿using SerieDLL_EF.BDD;
+using SerieDLL_EF.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerieDLL_EF.Repository
+{
+    public class BaseRepo<TClass> where TClass : class
+    {
+        protected BddprojetContext context;
+        protected Func<TClass, bool> predicate;
+
+        public BaseRepo()
+        {
+            context = new();
+        }
+
+        public void Add(TClass item)
+        {
+            context.Add(item);
+            context.SaveChanges();
+        }
+
+        public void Update(TClass item)
+        {
+            context.Update(item);
+            context.SaveChanges();
+        }
+
+        public void Delete(TClass item)
+        {
+            context.Remove(item);
+            context.SaveChanges();
+        }
+
+    }
+}
