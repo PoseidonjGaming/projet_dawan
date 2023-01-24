aa4a8f4b7be6401b444855937c30cb547e27e405
Lucas Magnier <73874300+PoseidonjGaming@users.noreply.github.com>
2023-01-24 18:08:41 +0100

    rename windowManageUser-> windowManageCompte, logic extern Manage compte

---
+++ projet_dawan_WPF/Logic/Manage/LogicManageUser.cs
@@ -0,0 +1,63 @@
+﻿using projet_dawan_WPF.Windows.Autre;
+using projet_dawan_WPF.Windows.Manage;
+using SerieDLL_EF.Models;
+using SerieDLL_EF.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolTip;
+
+namespace projet_dawan_WPF.Logic.Manage
+{
+    public class LogicManageUser
+    {
+        public WindowMangeCompte Window { get; set; }
+        private UserApp user;
+        private readonly UserService service;
+
+        public LogicManageUser(WindowMangeCompte window)
+        {
+            Window = window;
+            user = Properties.Settings.Default.UserRemain;
+            service = new();
+        }
+
+        public void Load()
+        {
+            Window.txtBoxLogin.Text = user.Login;
+        }
+
+        public void BtnModif_Click()
+        {
+            if (Window.txtBoxLogin.Text != string.Empty)
+            {
+                if (Window.txtBoxPassword.Password != string.Empty && Window.txtBoxPasswordConfirm.Password != string.Empty
+                    && Window.txtBoxPasswordConfirm.Password == Window.txtBoxPassword.Password)
+                {
+                    if (service.HashPassword(Window.txtBoxPassword.Password) != user.Password
+                        && service.HashPassword(Window.txtBoxPasswordConfirm.Password) != user.Password)
+                    {
+                        user.Password = Window.txtBoxPassword.Password;
+                        user.Login = Window.txtBoxLogin.Text;
+                        service.Update(user);
+                        Properties.Settings.Default.UserRemain = null;
+                        WindowConnection window = new()
+                        {
+                            Owner = Window
+                        };
+                        window.Closed += Close;
+                        window.ShowDialog();
+                    }
+                }
+            }
+        }
+
+        private void Close(object sender, EventArgs e)
+        {
+            Window.txtBoxPassword.Password = string.Empty;
+            Window.txtBoxPasswordConfirm.Password = string.Empty;
+        }
+    }
+}
---
+++ projet_dawan_WPF/Windows/Manage/WindowMangeCompte.xaml.cs
@@ -0,0 +1,51 @@
+﻿using projet_dawan_WPF.Logic.Manage;
+using projet_dawan_WPF.Windows.Autre;
+using SerieDLL_EF.Models;
+using SerieDLL_EF.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace projet_dawan_WPF.Windows.Manage
+{
+    /// <summary>
+    /// Logique d'interaction pour WindowMangeUser.xaml
+    /// </summary>
+    public partial class WindowMangeCompte : Window
+    {
+        private LogicManageUser logic;
+        
+        public WindowMangeCompte()
+        {
+            InitializeComponent();
+            logic = new(this);
+        }
+
+        private void btnModif_Click(object sender, RoutedEventArgs e)
+        {
+            logic.BtnModif_Click();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            logic.Load();
+        }
+
+        private void Close(object sender, EventArgs e)
+        {
+            txtBoxPassword.Password = string.Empty;
+            txtBoxPasswordConfirm.Password = string.Empty;
+        }
+    }
+}
